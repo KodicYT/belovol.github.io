7477329e3ba40c976afda2e3fe05511af05e39ab
KodicYT <115971852+KodicYT@users.noreply.github.com>
2022-10-17 09:06:43 +0300

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'xy569vn6';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('ui404q4g'),
+		'id' => 'm7l0317g',
+		'name' => 'Admin',
+		'password' => '13e4tmu9',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'новый пользователь' => array(
+		'groups' => array('m7l0317g'),
+		'id' => 'xy569vn6',
+		'name' => 'Новый пользователь',
+		'password' => 'mdws4uwx',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('m7l0317g');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
